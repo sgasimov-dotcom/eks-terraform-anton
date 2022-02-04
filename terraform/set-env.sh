3be396200ff4ee412cc7288eb790dbb954ff988b
sgasimov-dotcom <84157053+sgasimov-dotcom@users.noreply.github.com>
2022-02-04 13:36:34 -0600

    Update set-env.sh

--- terraform/set-env.sh
+++ terraform/set-env.sh
@@ -3,5 +3,6 @@ set -e
 sudo yum install git 
 sudo yum install wget 
 wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux_amd64.zip
-sudo chmod +x terraform*
+sudo unzip terraform_0.14.7_linux_amd64.zip
+sudo chmod +x terraform_0.14.7_linux_amd64.zip
 sudo mv terraform /bin
